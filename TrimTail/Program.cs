657109068b1654cdf871f8bdae48755b841640c9
Joseph Chen <joseph@baoruh.com.tw>
2024-05-31 16:27:50 +0800

    Use file-scoped namespace and Any for trailing blanks check

--- TrimTail/Program.cs
+++ TrimTail/Program.cs
@@ -1,97 +1,88 @@
-﻿namespace TrimTail
+﻿namespace TrimTail;
+
+public class Program
 {
-    public class Program
+    private static readonly SemaphoreSlim sem = new(1);
+
+    public static bool HasTrailingBlanks(in string filePath)
     {
-        private static readonly SemaphoreSlim sem = new(1);
+        return File.ReadLines(filePath).Any(line => !string.IsNullOrEmpty(line) && char.IsWhiteSpace(line[^1]));
+    }
 
-        public static bool HasTrailingBlanks(in string filePath)
+    public static void RemoveTrailingBlanks(in string filePath)
+    {
+        if (!HasTrailingBlanks(filePath)) return;
+
+        var tempPath = Path.GetTempFileName();
+        using (var origFile = File.OpenText(filePath))
+        using (var tempFile = File.CreateText(tempPath))
         {
-            foreach (var line in File.ReadLines(filePath))
+            origFile.BaseStream.Seek(-1, SeekOrigin.End);
+            bool endWithNewline = (origFile.BaseStream.ReadByte() == '\n');
+
+            origFile.BaseStream.Seek(0, SeekOrigin.Begin);
+            origFile.DiscardBufferedData();
+
+            string? line;
+            while ((line = origFile.ReadLine()) != null)
             {
-                if (!string.IsNullOrEmpty(line) && char.IsWhiteSpace(line[^1]))
+                line = line.TrimEnd();
+                if (!origFile.EndOfStream || endWithNewline)
                 {
-                    return true;
+                    tempFile.WriteLine(line);
+                }
+                else
+                {
+                    tempFile.Write(line);
                 }
             }
-
-            return false;
         }
-
-        public static void RemoveTrailingBlanks(in string filePath)
+        try
         {
-            if (!HasTrailingBlanks(filePath)) return;
-
-            var tempPath = Path.GetTempFileName();
-            using (var origFile = File.OpenText(filePath))
-            using (var tempFile = File.CreateText(tempPath))
-            {
-                origFile.BaseStream.Seek(-1, SeekOrigin.End);
-                bool endWithNewline = (origFile.BaseStream.ReadByte() == '\n');
-
-                origFile.BaseStream.Seek(0, SeekOrigin.Begin);
-                origFile.DiscardBufferedData();
-
-                string? line;
-                while ((line = origFile.ReadLine()) != null)
-                {
-                    line = line.TrimEnd();
-                    if (!origFile.EndOfStream || endWithNewline)
-                    {
-                        tempFile.WriteLine(line);
-                    }
-                    else
-                    {
-                        tempFile.Write(line);
-                    }
-                }
-            }
-            try
-            {
-                File.Replace(tempPath, filePath, null);
-            }
-            catch (IOException)
-            {
-                File.Copy(tempPath, filePath, true);
-                File.Delete(tempPath);
-            }
+            File.Replace(tempPath, filePath, null);
         }
-
-        public static void ProcessDir(string dirPath, HashSet<string> allowedExts)
+        catch (IOException)
         {
-            Console.WriteLine($"Processing directory: {dirPath}");
-
-            var files = Directory.EnumerateFiles(dirPath, "*", SearchOption.AllDirectories)
-                .AsParallel()
-                .Where(filePath => allowedExts.Contains(Path.GetExtension(filePath).ToLower()));
-
-            Parallel.ForEach(files, filePath =>
-            {
-                RemoveTrailingBlanks(filePath);
-                sem.Wait();
-                Console.WriteLine(Path.GetRelativePath(dirPath, filePath));
-                sem.Release();
-            });
+            File.Copy(tempPath, filePath, true);
+            File.Delete(tempPath);
         }
+    }
 
-        static void Main(string[] args)
+    public static void ProcessDir(string dirPath, HashSet<string> allowedExts)
+    {
+        Console.WriteLine($"Processing directory: {dirPath}");
+
+        var files = Directory.EnumerateFiles(dirPath, "*", SearchOption.AllDirectories)
+            .AsParallel()
+            .Where(filePath => allowedExts.Contains(Path.GetExtension(filePath).ToLower()));
+
+        Parallel.ForEach(files, filePath =>
         {
-            var start = DateTime.Now;
-            HashSet<string> allowedExts;
+            RemoveTrailingBlanks(filePath);
+            sem.Wait();
+            Console.WriteLine(Path.GetRelativePath(dirPath, filePath));
+            sem.Release();
+        });
+    }
 
-            if (args.Length > 0)
-            {
-                allowedExts = args.Select(x => x.ToLower()).ToHashSet();
-            }
-            else
-            {
-                allowedExts = [".h", ".c", ".hpp", ".cpp"];
-            }
+    static void Main(string[] args)
+    {
+        var start = DateTime.Now;
+        HashSet<string> allowedExts;
 
-            ProcessDir(Directory.GetCurrentDirectory(), allowedExts);
-
-            var end = DateTime.Now;
-            var duration = (end - start).TotalMilliseconds;
-            Console.WriteLine($"Elapsed time: {duration} ms");
+        if (args.Length > 0)
+        {
+            allowedExts = args.Select(x => x.ToLower()).ToHashSet();
         }
+        else
+        {
+            allowedExts = [".h", ".c", ".hpp", ".cpp"];
+        }
+
+        ProcessDir(Directory.GetCurrentDirectory(), allowedExts);
+
+        var end = DateTime.Now;
+        var duration = (end - start).TotalMilliseconds;
+        Console.WriteLine($"Elapsed time: {duration} ms");
     }
 }
--- TrimTailTests/TrimTailTests.cs
+++ TrimTailTests/TrimTailTests.cs
@@ -1,57 +1,56 @@
 using TrimTail;
 
-namespace TrimTailTests
+namespace TrimTailTests;
+
+[TestClass]
+public class TrimTailTests
 {
-    [TestClass]
-    public class TrimTailTests
+    const string filePath = "test.txt";
+
+    [TestMethod]
+    public void TestHasTrailingBlanks()
     {
-        const string filePath = "test.txt";
+        File.WriteAllText(filePath, "    ");
+        Assert.IsTrue(Program.HasTrailingBlanks(filePath));
+        File.Delete(filePath);
 
-        [TestMethod]
-        public void TestHasTrailingBlanks()
-        {
-            File.WriteAllText(filePath, "    ");
-            Assert.IsTrue(Program.HasTrailingBlanks(filePath));
-            File.Delete(filePath);
+        File.WriteAllText(filePath, "Test w/ trailing spaces    ");
+        Assert.IsTrue(Program.HasTrailingBlanks(filePath));
+        File.Delete(filePath);
 
-            File.WriteAllText(filePath, "Test w/ trailing spaces    ");
-            Assert.IsTrue(Program.HasTrailingBlanks(filePath));
-            File.Delete(filePath);
+        File.WriteAllText(filePath, "Test w/ trailing tabs\t\t");
+        Assert.IsTrue(Program.HasTrailingBlanks(filePath));
+        File.Delete(filePath);
 
-            File.WriteAllText(filePath, "Test w/ trailing tabs\t\t");
-            Assert.IsTrue(Program.HasTrailingBlanks(filePath));
-            File.Delete(filePath);
+        File.WriteAllText(filePath, "Test w/o trailing blanks");
+        Assert.IsFalse(Program.HasTrailingBlanks(filePath));
+        File.Delete(filePath);
+    }
 
-            File.WriteAllText(filePath, "Test w/o trailing blanks");
-            Assert.IsFalse(Program.HasTrailingBlanks(filePath));
-            File.Delete(filePath);
-        }
+    [TestMethod]
+    public void TestRemoveTrailingBlanks()
+    {
+        File.WriteAllText(filePath, "Test w/ trailing blanks    ");
+        Program.RemoveTrailingBlanks(filePath);
+        Assert.AreEqual("Test w/ trailing blanks", File.ReadAllText(filePath));
+        File.Delete(filePath);
 
-        [TestMethod]
-        public void TestRemoveTrailingBlanks()
-        {
-            File.WriteAllText(filePath, "Test w/ trailing blanks    ");
-            Program.RemoveTrailingBlanks(filePath);
-            Assert.AreEqual("Test w/ trailing blanks", File.ReadAllText(filePath));
-            File.Delete(filePath);
+        File.WriteAllText(filePath, "Test w/ trailing blanks and newline    \r\n");
+        Program.RemoveTrailingBlanks(filePath);
+        Assert.AreEqual("Test w/ trailing blanks and newline\r\n", File.ReadAllText(filePath));
+        File.Delete(filePath);
+    }
 
-            File.WriteAllText(filePath, "Test w/ trailing blanks and newline    \r\n");
-            Program.RemoveTrailingBlanks(filePath);
-            Assert.AreEqual("Test w/ trailing blanks and newline\r\n", File.ReadAllText(filePath));
-            File.Delete(filePath);
-        }
-
-        [TestMethod]
-        public void TestProcessDir()
-        {
-            const string dirPath = "testDir";
-            Directory.CreateDirectory(dirPath);
-            File.WriteAllText(Path.Combine(dirPath, "test1.txt"), "This is a test with trailing blanks    ");
-            File.WriteAllText(Path.Combine(dirPath, "test2.txt"), "Another test with trailing blanks    ");
-            Program.ProcessDir(dirPath, [".txt"]);
-            var files = Directory.EnumerateFiles(dirPath, "*", SearchOption.AllDirectories);
-            Assert.IsTrue(files.All(filePath => !File.ReadAllText(filePath).EndsWith(' ')));
-            Directory.Delete(dirPath, true);
-        }
+    [TestMethod]
+    public void TestProcessDir()
+    {
+        const string dirPath = "testDir";
+        Directory.CreateDirectory(dirPath);
+        File.WriteAllText(Path.Combine(dirPath, "test1.txt"), "This is a test with trailing blanks    ");
+        File.WriteAllText(Path.Combine(dirPath, "test2.txt"), "Another test with trailing blanks    ");
+        Program.ProcessDir(dirPath, [".txt"]);
+        var files = Directory.EnumerateFiles(dirPath, "*", SearchOption.AllDirectories);
+        Assert.IsTrue(files.All(filePath => !File.ReadAllText(filePath).EndsWith(' ')));
+        Directory.Delete(dirPath, true);
     }
 }
